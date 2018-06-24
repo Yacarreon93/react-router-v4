395d6d3b856bce2bc12260987128307d99777701
Yasser Carreón <yacasia@gmail.com>
2018-06-24 14:16:18 -0500

    Create router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => <h1>Hello</h1>;
+const Home = () => <h1>Home</h1>;
+
+const About = () => <h1>About</h1>;
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path="/" component={Home} /> 
+      <Route path="/about" component={About} /> 
+    </div>
+  </Router>
+);
 
 export default App;
