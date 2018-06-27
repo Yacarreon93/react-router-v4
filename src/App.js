8b88d1c3d7849f393643c5ab140a05a3f6dd52fc
Yasser Carreón <yacasia@gmail.com>
2018-06-27 15:10:52 -0500

    Include NavLink examples

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import {
   BrowserRouter as Router, 
   Route,
   Link,
+  NavLink,
 } from 'react-router-dom';
+
+import './App.css';
+
+const isActiveFunc = (match, location) => {
+  console.log(match, location);
+  return match;
+};
 
 const Home = () => <h1>Home</h1>;
 
 const About = () => <h1>About</h1>;
+
+const NavLinks = () => (
+  <nav>
+    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+    <NavLink activeStyle={{ color: 'green' }} to={{ pathname: '/about' }}>About</NavLink>
+    <NavLink 
+      isActive={isActiveFunc} 
+      activeClassName="active"
+      to={{ pathname: '/about/sub' }}
+    >
+      About Sub
+    </NavLink>
+  </nav>
+);
 
 const Links = () => (
   <nav>
@@ -21,7 +43,7 @@ const Links = () => (
 const App = () => (
   <Router>
     <div>
-      <Links />
+      <NavLinks />
       <Route exact path="/" component={Home} /> 
       <Route path="/about" component={About} /> 
       <Route strict path="/contact/" render={() => <h1>Contact</h1>} /> 
