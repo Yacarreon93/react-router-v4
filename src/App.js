a571a4f7b6adb157454eb5ecf7d8193d328d26cd
Yasser Carreón <yacasia@gmail.com>
2018-06-30 11:24:07 -0500

    Create different types of routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { 
+  BrowserRouter,
+  HashRouter,
+  MemoryRouter,
+  StaticRouter,
+  NativeRouter,
   BrowserRouter as Router, 
   Route,
   Link,
@@ -160,6 +165,50 @@ const App = () => (
       <Body />
     </div>
   </Router>
+);
+
+const LinksRoutes = () => (
+  <div>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Route exact path="/" render={() => <h1>Home</h1>} />
+    <Route path="/about" render={() => <h1>About</h1>} />
+  </div>
+);
+
+const forceRefresh = () => {
+  console.log(new Date());
+  // return false;
+  return true;
+};
+
+const BrowserRouterApp = () => (
+  <BrowserRouter forceRefresh={forceRefresh()}>
+    <LinksRoutes />
+  </BrowserRouter>
+);
+
+const HashRouterApp = () => (
+  // <HashRouter hashType={'noslash'}>
+  // <HashRouter hashType={'hashbang'}>
+  <HashRouter>
+    <LinksRoutes />
+  </HashRouter>
+);
+
+const MemoryRouterApp = () => (
+  <MemoryRouter
+    initialEntries={['/', '/about']}
+    initialIndex={1}
+  >
+    <LinksRoutes />
+  </MemoryRouter>
+);
+
+const StaticRouterApp = () => (
+  <StaticRouter location="/about" context={{}}>
+    <LinksRoutes />
+  </StaticRouter>
 );
 
 export default App;
